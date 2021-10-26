16d3983c5937c27cb3cc84cb5b6a083e2bf2a8aa
Biswajit Behera <36480889+biswa1751@users.noreply.github.com>
2021-10-26 11:32:23 +0530

    add types

--- src/resources/contacts.ts
+++ src/resources/contacts.ts
@@ -1,9 +1,38 @@
+import { Notes } from '../types/types';
 import AxiosClient from '../utils/axios_client';
 
+enum ContactType {
+  vendor = 'vendor',
+  customer = 'customer',
+  employee = 'employee',
+  self = 'self',
+}
+interface CreateContactParams {
+  name: string;
+  email?: string;
+  contact?: string;
+  type?: ContactType | string;
+  reference_id?: string;
+  notes: Notes;
+}
+
+interface CreateContactResponse {
+  id: string;
+  entity: string;
+  name: string;
+  contact: string;
+  email: string;
+  type: string;
+  reference_id: string;
+  batch_id: string;
+  active: boolean;
+  notes: Notes;
+  created_at: number;
+}
 export default function contacts(axiosClient: AxiosClient) {
   return {
-    async create(params: any) {
-      return axiosClient.post({ url: '/contacts', data: params });
+    async create(params: CreateContactParams) {
+      return axiosClient.post<CreateContactResponse>({ url: '/contacts', data: params });
     },
   };
 }
---
+++ src/types/types.ts
@@ -0,0 +1,3 @@
+export interface Notes {
+  [key: string]: string;
+}
